5e53dea0676cb051ede293662800e6623f1da880
Matthew Davis <mdavis0351@gmail.com>
2022-10-13 23:09:12 -0500

    add step to set proper vars on keyfile

--- .github/config/decrypt_secret.sh
+++ .github/config/decrypt_secret.sh
@@ -6,3 +6,5 @@ mkdir $HOME/secrets
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SSH_PASSPHRASE" \
 --output $HOME/secrets/ansible-key ansible-key.gpg
+
+chmod 600 $HOME/secrets/ansible-key
